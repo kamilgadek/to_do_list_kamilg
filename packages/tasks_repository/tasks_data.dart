55918bb09f56069a0d114f481c7befc4fff2ecd1
Kamil Gadek <kamil.gadek124@gmail.com>
2024-08-20 11:54:19 +0200

    Create list of tasks

---
+++ packages/tasks_repository/tasks_data.dart
@@ -0,0 +1,29 @@
+class Task {
+  final String title;
+  final String description;
+  final bool isDone;
+
+  Task({
+    required this.title,
+    required this.description,
+    required this.isDone,
+  });
+}
+
+List<Task> tasks = [
+  Task(
+    title: 'Task 1', 
+    description: 'Lear Flutter', 
+    isDone: false,
+  ),
+  Task(
+    title: 'Task 2', 
+    description: 'Clean room', 
+    isDone: true,
+  ),
+  Task(
+    title: 'Task 1', 
+    description: 'Go to the gym', 
+    isDone: false,
+  ),
+];
