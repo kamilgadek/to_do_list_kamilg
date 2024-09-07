73ae07957c411bbb158d8011e421a48abeda8b97
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-07 19:55:38 +0200

    Rename file name to task_card.dart

---
+++ lib/home/widgets/task_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:tasks_repository/tasks_repository.dart';
+
+
+class TaskCard extends StatelessWidget {
+  const TaskCard(
+     {
+    super.key, required this.task,
+  });
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.amber,
+      ),
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Text(
+            task.description,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10,),
+          Text(
+            task.isDone ? "Completed" : "Incomplete", 
+            style: TextStyle(
+              fontSize: 16,
+              color: task.isDone ? Colors.green : Colors.red,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
