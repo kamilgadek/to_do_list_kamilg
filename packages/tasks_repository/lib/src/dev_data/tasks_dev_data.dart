4ad58e90be26a6619f0dedf725c5316907dca3f7
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-18 20:54:10 +0200

    refactor: Make code look nicer

--- packages/tasks_repository/lib/src/dev_data/tasks_dev_data.dart
+++ packages/tasks_repository/lib/src/dev_data/tasks_dev_data.dart
@@ -4,14 +4,38 @@ import 'package:tasks_repository/tasks_repository.dart';
 /// TODO: Remove it later as it's for dev purposes only.
 class TasksDevData {
   static List<Task> tasks = [
-    Task(description: 'Learn Flutter', isDone: true),
-    Task(description:'Clean room', isDone: false),
-    Task(description:'Go to the gym', isDone: true),
-    Task(description:'Learn Flutter',isDone: true),
-    Task(description:'Clean room',isDone: false),
-    Task(description:'Go to the gym',isDone: true),
-    Task(description:'Learn Flutter',isDone: true),
-    Task(description:'Clean room',isDone: false),
-    Task(description:'Go to the gym',isDone: true),
+    Task(
+      description: 'Learn Flutter',
+      isDone: true,
+    ),
+    Task(
+      description: 'Clean room',
+      isDone: false,
+    ),
+    Task(
+      description: 'Go to the gym',
+      isDone: true,
+    ),
+    Task(
+      description: 'Learn Flutter',
+      isDone: true,
+    ),
+    Task(
+      description: 'Clean room',
+      isDone: false,
+    ),
+    Task(
+      description: 'Go to the gym',
+      isDone: true,
+    ),
+    Task(
+      description: 'Learn Flutter',
+      isDone: true,
+    ),
+    Task(
+      description: 'Clean room',
+      isDone: false,
+    ),
+    Task(description: 'Go to the gym', isDone: true),
   ];
 }
