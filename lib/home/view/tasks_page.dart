50cb924fb963d8736bed62960d254719576f72e2
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-23 18:47:49 +0200

    refactor: move floating action button to widgets folder

--- lib/home/view/tasks_page.dart
+++ lib/home/view/tasks_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tasks_repository/tasks_repository.dart';
 import 'package:to_do_list_kamilg/home/home.dart';
-import 'package:to_do_list_kamilg/home/view/fab_view.dart';
+import 'package:to_do_list_kamilg/home/widgets/fab_view.dart';
 
 class TasksPage extends StatelessWidget {
   const TasksPage({
