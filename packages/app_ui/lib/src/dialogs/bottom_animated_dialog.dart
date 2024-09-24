fe02390c5ae1ad0edee2ff3153ccaa64e3b93e96
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-24 20:40:53 +0200

    refactor(app_ui): upload BottomSheetDialog

--- packages/app_ui/lib/src/dialogs/bottom_animated_dialog.dart
+++ packages/app_ui/lib/src/dialogs/bottom_animated_dialog.dart
@@ -3,19 +3,33 @@ import 'package:flutter/material.dart';
 class BottomAnimatedDialog {
   static void show(BuildContext context, {required Widget child}) {
     showGeneralDialog(
-        barrierLabel: 'BottomAnimatedDialog',
-        barrierDismissible: true,
-        barrierColor: Colors.black.withOpacity(0.5),
-        transitionDuration: Duration(milliseconds: 500),
-        context: context,
-        pageBuilder: (context, anim1, anim2) {
-          return Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 300,
-              child: child,
+      barrierLabel: 'BottomAnimatedDialog',
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.5),
+      transitionDuration: const Duration(milliseconds: 500),
+      context: context,
+      pageBuilder: (context, anim1, anim2) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 300,
+            width: double.infinity,
+            margin: EdgeInsets.only(bottom: 20, left: 12, right: 12),
+            child: child,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(40),
             ),
-          );
-        });
+          ),
+        );
+      },
+      transitionBuilder: (context, anim1, anim2, child) {
+        return SlideTransition(
+          position: Tween(begin: const Offset(0, 1), end: const Offset(0, 0))
+              .animate(anim1),
+          child: child,
+        );
+      },
+    );
   }
 }
