33a912c98582a61cfdcdfa676749011f1cd0c1bc
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-29 13:36:36 +0200

    refactor(app_ui): rename app colors

--- packages/app_ui/lib/src/colors/app_colors.dart
+++ packages/app_ui/lib/src/colors/app_colors.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primaryColor = Color.fromARGB(0, 111, 119, 239);
-  static const Color secondaryColor = Color.fromARGB(0, 68, 20, 241);
+  static const Color blue= Color.fromARGB(0, 111, 119, 239);
+  static const Color yellow = Color.fromARGB(0, 241, 241, 20);
   static const Color white = Color.fromARGB(255, 255, 255, 255);
 }
