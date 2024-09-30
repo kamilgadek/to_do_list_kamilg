8391f120d261af63331e7dd0cff607c5ad692c7e
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-30 19:07:58 +0200

    refactor(app_theme): rename  theme to 'light' for future dark mode support

--- packages/app_ui/lib/src/theme/app_theme.dart
+++ packages/app_ui/lib/src/theme/app_theme.dart
@@ -2,7 +2,7 @@ import 'package:app_ui/src/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData get normal {
+  static ThemeData get light {
     return ThemeData(
       fontFamily: 'Poppins',
       scaffoldBackgroundColor: AppColors.white,
