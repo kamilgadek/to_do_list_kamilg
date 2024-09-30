6a0ef360748d122bc593055ac8bd18a3cd6751d3
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-30 19:37:39 +0200

    refactor(app_ui): move text styles to separate folder and make barell

--- packages/app_ui/lib/app_ui.dart
+++ packages/app_ui/lib/app_ui.dart
@@ -2,4 +2,5 @@ library;
 export 'src/dialogs/dialogs.dart';
 export 'src/sizes/sizes.dart';
 export 'src/theme/theme.dart';
-
+export 'src/const/const.dart';
+export 'src/styles/styles.dart';
---
+++ packages/app_ui/lib/src/const/const.dart
@@ -0,0 +1,1 @@
+export './app_ui_const.dart';
