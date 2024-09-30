f4b2d3f8135bba65dc617878309a8b2bc8185ac1
Kamil Gadek <kamil.gadek124@gmail.com>
2024-09-30 20:50:04 +0200

    refactor(app_theme): extract font family to a static constant

--- packages/app_ui/lib/src/theme/app_theme.dart
+++ packages/app_ui/lib/src/theme/app_theme.dart
@@ -1,12 +1,10 @@
-import 'package:app_ui/src/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+static const _fontFamily = 'Poppins';
   static ThemeData get light {
     return ThemeData(
-      fontFamily: 'Poppins',
-      scaffoldBackgroundColor: AppColors.white,
+      fontFamily: _fontFamily,
     );
-  
   }
 }
