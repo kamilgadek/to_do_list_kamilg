ef859ca9a868053831fb52a711acc7530d372379
Kamil Gadek <kamil.gadek124@gmail.com>
2024-11-23 16:53:24 +0100

    refactor(database): move database name from main to TasksRepository

--- lib/main.dart
+++ lib/main.dart
@@ -4,40 +4,6 @@ import 'package:to_do_list_kamilg/app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final localStorage = LocalStorage(databaseName: 'tasks.db');
 
-  // Initialize the database
-  await localStorage.getDatabase();
-
-  // Add task
-  await localStorage.addDocument(collection: 'tasks', document: {
-    'content': 'Buy milk',
-    'status': 0
-  });
-  print("Task added.");
-
-  // Get all tasks
-  final tasks = await localStorage.getDocuments(collection: 'tasks');
-  print("All tasks: $tasks");
-
-  // Edit task
-  if (tasks.isNotEmpty) {
-    final taskId = tasks.first['id'];
-    await localStorage.editTask(taskId, "Updated Task", 1);
-    print("Task edited.");
-
-    // Get tasks afrted edited
-    final updatedTasks = await localStorage.getDocuments(collection: 'tasks');
-    print("Tasks after edit: $updatedTasks");
-
-    // Delete task
-    await localStorage.deleteTask(taskId);
-    print("Task deleted.");
-
-    // Get tesks after deleted
-    final tasksAfterDelete = await localStorage.getDocuments(collection: 'tasks');
-    print("Tasks after delete: $tasksAfterDelete");
-  }
-
-  runApp(const App());
+  runApp(App());
 }
